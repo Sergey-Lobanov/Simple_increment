5fb83bd95f5be4411b992120f403f8ca30e4bbcb
Sergey-Lobanov <88037824+Sergey-Lobanov@users.noreply.github.com>
2022-10-06 00:54:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple_increment
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-cdkx6t)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  public i = 0;
+
+  public increment() {
+    this.i++;
+  }
+}
